932088457f050cc355c7d18517e4bff7209ceb7c
returnitems <diwan.umang@gmail.com>
2024-08-02 21:13:29 -0400

    Working on routes

--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
 import { Router } from "express";
+import * as playerController from "../Controllers/playerController.js";
 
 const router = Router();
 
+router.get('/', playerController.getPlayers);
+router.get('/:playerId', playerController.getSinglePlayer);
